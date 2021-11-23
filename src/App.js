948e69d0060b70b2b1ce7c544290f2f43b64d250
ShafiaChy <shafiachy04@gmail.com>
2021-11-23 18:50:58 +0600

    readme file updated

--- src/App.js
+++ src/App.js
@@ -19,16 +19,16 @@ function App() {
         <Route exact path="/">
           <Home></Home>
         </Route>
-        <Route path="/home">
+        <Route exact path="/home">
           <Home></Home>
         </Route>
-        <Route path="/about">
+        <Route exact path="/about">
           <About></About>
         </Route>
-        <Route path="/services">
+        <Route exact path="/services">
           <Services></Services>
         </Route>
-        <Route path="/teach">
+        <Route exact path="/teach">
          <TeacherOnStudyBuddies></TeacherOnStudyBuddies>
         </Route>
         <Route path="/*">
